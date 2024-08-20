c2298ed8e93caedb26ff12e59da126930f4c1227
Thiago N Vasque <thiagonvasque@gmail.com>
2024-08-20 20:40:49 -0300

    Inserção da tela de escolha Doutor/Paciente e logo

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,12 @@ use App\Http\Controllers\PacienteController;
 use App\Http\Controllers\DoutorController;
 use App\Http\Controllers\ZoomController;
 
+
+//Rota para o home/tela inicial
+Route::get('/', function(){
+    return view('home');
+});
+
 //Rotas para o paciente
 Route::get('/paciente/sessoes', [PacienteController::class, 'sessoes'])->name('paciente.sessoes');
 Route::get('/paciente/anotacoes', [PacienteController::class, 'anotacoes'])->name('paciente.anotacoes');
@@ -12,6 +18,7 @@ Route::get('/paciente/pagamentos', [PacienteController::class, 'pagamentos'])->n
 Route::get('/paciente/historico', [PacienteController::class, 'historico'])->name('paciente.historico');
 
 //Rotas para o doutor
+Route::get('/doutor', [DoutorController::class, 'login'])->name('doutor.login');
 Route::get('/doutor/pacientes', [DoutorController::class, 'index'])->name('doutor.pacientes');
 Route::get('/doutor/sessoes', [DoutorController::class, 'sessoes'])->name('doutor.sessoes');
 Route::get('/doutor/relatorios', [DoutorController::class, 'relatorios'])->name('doutor.relatorios');
